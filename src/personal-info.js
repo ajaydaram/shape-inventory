478a95070aab88342ccb925808553ef7acf37095
Ajay <user@Ajays-MacBook-Pro.local>
2023-05-21 17:39:00 +0800

    first commit

---
+++ src/personal-info.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class PersonalInfo extends React.Component {
+    render() {
+        return (
+        <div class="personal-info-container">
+            <h3 class="personal-info-heading">PERSONAL INFORMATION</h3>
+            <h2 class="main-heading">S.H.A.P.E INVENTORY</h2>
+            <div>
+                <h2>Personal information</h2>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default PersonalInfo;
